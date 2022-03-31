30042f59e8affca85926d33ee7b36d77da251cd4
Neto002 <antoniogfneto92@hotmail.com>
2022-03-31 13:28:26 -0300

    update

---
+++ .next/server/pages/users/create.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/users/create";
+exports.ids = ["pages/users/create"];
+exports.modules = {
+
+/***/ "./src/pages/users/create.tsx":
+/*!************************************!*\
+  !*** ./src/pages/users/create.tsx ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nconst UserCreatePage = ()=>{\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: \"Cadastro\"\n    }, void 0, false, {\n        fileName: \"/home/neto/Projects/NextJs/nextjs/src/pages/users/create.tsx\",\n        lineNumber: 3,\n        columnNumber: 9\n    }, undefined);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (UserCreatePage);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvcGFnZXMvdXNlcnMvY3JlYXRlLnRzeC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTtBQUFBLE1BQU1BLGNBQWMsR0FBRyxJQUFNO0lBQ3pCLHFCQUNJLDhEQUFDQyxJQUFFO2tCQUFDLFVBQVE7Ozs7O2lCQUFLLENBQ25CO0NBQ0w7QUFFRCxpRUFBZUQsY0FBYyIsInNvdXJjZXMiOlsid2VicGFjazovL25leHRqcy8uL3NyYy9wYWdlcy91c2Vycy9jcmVhdGUudHN4PzA0ZmMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgVXNlckNyZWF0ZVBhZ2UgPSAoKSA9PiB7XG4gICAgcmV0dXJuIChcbiAgICAgICAgPGgxPkNhZGFzdHJvPC9oMT5cbiAgICApO1xufVxuXG5leHBvcnQgZGVmYXVsdCBVc2VyQ3JlYXRlUGFnZVxuIl0sIm5hbWVzIjpbIlVzZXJDcmVhdGVQYWdlIiwiaDEiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/pages/users/create.tsx\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./src/pages/users/create.tsx"));
+module.exports = __webpack_exports__;
+
+})();
